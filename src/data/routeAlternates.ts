03854a287e908b91050a249c4da1b5ddc37a0a75
Badukeando <eloy.martin@pp-systeme.de>
2026-03-27 11:25:20 +0100

    fix hreflang routing and add 404 page

---
+++ src/data/routeAlternates.ts
@@ -0,0 +1,33 @@
+import { faqPageContent } from "./faqPages";
+import { tattooAftercareGuide } from "./guidePages";
+import { servicePageContent, type ServicePageKey } from "./servicePages";
+import type { SiteLang } from "./site";
+
+export type RouteAlternates = Record<SiteLang, string>;
+
+export const homeRouteAlternates: RouteAlternates = {
+  es: "/",
+  en: "/en/",
+};
+
+export const worksRouteAlternates: RouteAlternates = {
+  es: "/works",
+  en: "/en/works",
+};
+
+export const faqRouteAlternates: RouteAlternates = {
+  es: faqPageContent.es.path,
+  en: faqPageContent.en.path,
+};
+
+export const guideRouteAlternates: RouteAlternates = {
+  es: tattooAftercareGuide.es.path,
+  en: tattooAftercareGuide.en.path,
+};
+
+export const getServiceRouteAlternates = (
+  pageKey: ServicePageKey,
+): RouteAlternates => ({
+  es: servicePageContent.es[pageKey].path,
+  en: servicePageContent.en[pageKey].path,
+});
